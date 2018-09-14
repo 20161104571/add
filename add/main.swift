4d2783a1cbdc6a52ffffc89cfc8ee1633505a845
wei <a20161104571@mac18.local>
2018-09-14 17:35:05 +0800

    Initial Commit

---
+++ add/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  add
+//
+//  Created by 20161104571 on 18/9/14.
+//  Copyright © 2018年 20161104571. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
